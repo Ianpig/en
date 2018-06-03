942a17bd48c26a2a8148d0ddd4460bad4655bbe9
Ianpig <chu2815@hotmail.com>
2018-06-04 00:46:54 +0800

    Site updated: 2018-06-04 00:46:53

--- js/all.js
+++ js/all.js
@@ -1,3 +1,14 @@
 document.querySelector('.btn-menu').addEventListener('click',function(e){
   document.querySelector('body').classList.toggle('show');
 });
+
+var navHeight = document.querySelector('#nav-menu').offsetHeight;
+var navTop = document.querySelector('#nav-menu').offsetTop;
+
+document.addEventListener('scroll',function(e){
+    if(window.pageYOffset > navHeight+navTop){
+      document.querySelector('.btn-menu').classList.add('active');
+    }else {
+      document.querySelector('.btn-menu').classList.remove('active');  
+    }
+});
